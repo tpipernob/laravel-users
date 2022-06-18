6fd307822dfbc39a168ea196af8cd314d6b908c8
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-06-18 15:16:16 -0300

    filter user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,6 +8,11 @@
 
 <h3><a href="{{ route('users.create') }}">Criar novo usuário</a></h3>
 
+<form action="{{ route('users.index') }}" method="get">
+    <input type="text" name="search" placeholder="Pesquisar">
+    <button>Pesquisar</button>
+</form>
+
 <ul>
     @foreach ($users as $user)
 
