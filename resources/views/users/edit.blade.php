bf601e5aa251d62f465265a9d2e88d1e076b6eda
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-06-18 15:54:52 -0300

    refactoring

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,20 +6,11 @@
 
 <h1>Editar Usuário - {{ $user->name }}</h1>
 
-@if ($errors->any())
-    <ul class="errors">
-        @foreach ($errors->all() as $error)
-            <li class="error">{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+@include('includes.validations-form')
 
 <form action="{{ route('users.update', $user->id )}}" method="post">
     @method('PUT')
-    @csrf
-    <input type="text" name="name" placeholder="Nome" value="{{ $user->name }}">
-    <input type="email" name="email" placeholder="E-mail" value="{{ $user->email }}">
-    <input type="password" name="password" placeholder="Senha">
+    @include('users._partials.form')
     <button type="submit">Editar</button>
 </form>
 
