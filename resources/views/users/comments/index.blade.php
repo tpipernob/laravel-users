f62d54f4e794bac83efae26c535e4a6e0f53130f
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-06-20 13:45:31 -0300

    creating and listing comments

--- resources/views/users/comments/index.blade.php
+++ resources/views/users/comments/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
-@section('title', 'Listagem dos usuários')
+@section('title', 'Comentários do Usuário')
 
 @section('content')
+
+    <h1 class="font-bold mb-6">Comentários do usuário {{ $user->name }}</h1>
 
     <form action="{{ route('users.index') }}" method="get">
         <label class="relative">
@@ -23,10 +25,10 @@
             <thead class="text-xs text-gray-700 uppercase bg-sky-100">
                 <tr>
                     <th scope="col" class="px-6 py-3">
-                        Nome
+                        Conteúdo
                     </th>
                     <th scope="col" class="px-6 py-3">
-                        Email
+                        Visível
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Ação
@@ -34,18 +36,17 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($users as $user)
+                @foreach ($comments as $comment)
                 <tr class="border-b odd:bg-white even:bg-gray-50">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                        {{ $user->name }}
+                        {{ $comment->body }}
                     </th>
                     <td class="px-6 py-4">
-                        {{ $user->email }}
+                        {{ $comment->visible }}
                     </td>
                     <td class="px-6 py-4">
                         <a href="{{ route('users.show', $user->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Detalhes</a> |
-                        <a href="{{ route('users.edit', $user->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a> |
-                        <a href="{{ route('comments.index', $user->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Comentários (0)</a>
+                        <a href="{{ route('users.edit', $user->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
                     </td>
                 </tr>
                 @endforeach
