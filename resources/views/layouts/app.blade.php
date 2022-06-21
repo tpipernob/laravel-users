ffd89f0f776e53735b3d95d4bc822218042a117a
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2022-06-21 09:34:40 -0300

    added authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,24 +9,31 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
 
     <header class="text-slate-50 body-font bg-sky-600">
         <div class="container mx-auto flex flex-wrap p-5 pl-0 flex-col md:flex-row items-center">
-          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span class="ml-3 text-xl text-slate-50">Gestão de usuários</span>
-          </a>
-          <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <a href="{{ route('users.index') }}" class="mr-5 hover:text-gray-600">Consultar Usuários</a>
-            <a href="{{ route('users.create') }}" class="mr-5 hover:text-gray-900">Cadastrar Usuários</a>
-          </nav>
-          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 text-gray-900">Sair
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+            <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <span class="ml-3 text-xl text-slate-50">Gestão de usuários</span>
+            </a>
+            <nav
+                class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                <a href="{{ route('users.index') }}" class="mr-5 hover:text-gray-600">Consultar Usuários</a>
+                <a href="{{ route('users.create') }}" class="mr-5 hover:text-gray-900">Cadastrar Usuários</a>
+            </nav>
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button
+                    class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 text-gray-900">Sair
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                </button>
+            </form>
         </div>
-      </header>
+    </header>
 
 
     <div class="container mx-auto px-4 m-4">
@@ -33,4 +41,5 @@
     </div>
 
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,22 +7,28 @@ use App\Http\Controllers\Admin\CommentController;
 use App\Models\Comment;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users/{id}/create', [CommentController::class, 'create']) -> name('comments.create');
-Route::post('/users/{id}/comments', [CommentController::class, 'store']) -> name('comments.store');
-Route::get('/users/{id}/comments', [CommentController::class, 'index']) -> name('comments.index');
-Route::get('/users/{userId}/comments/{id}', [CommentController::class, 'edit']) -> name('comments.edit');
-Route::put('/users/comments/{id}', [CommentController::class, 'update']) -> name('comments.update');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/users/{id}/create', [CommentController::class, 'create']) -> name('comments.create');
+    Route::post('/users/{id}/comments', [CommentController::class, 'store']) -> name('comments.store');
+    Route::get('/users/{id}/comments', [CommentController::class, 'index']) -> name('comments.index');
+    Route::get('/users/{userId}/comments/{id}', [CommentController::class, 'edit']) -> name('comments.edit');
+    Route::put('/users/comments/{id}', [CommentController::class, 'update']) -> name('comments.update');
+
+    Route::delete('/users/{id}', [UserController::class, 'destroy']) -> name('users.destroy');
+    Route::put('/users/{id}', [UserController::class, 'update']) -> name('users.update');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit']) -> name('users.edit');
+    Route::get('/users', [UserController::class, 'index']) -> name('users.index');
+    Route::get('/users/create', [UserController::class, 'create']) -> name('users.create'); //tem que ser ser antes da show
+    Route::post('/users/', [UserController::class, 'store']) -> name('users.store');
+    Route::get('/users/{id}', [UserController::class, 'show']) -> name('users.show');
+});
 
 
-Route::delete('/users/{id}', [UserController::class, 'destroy']) -> name('users.destroy');
-Route::put('/users/{id}', [UserController::class, 'update']) -> name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit']) -> name('users.edit');
-Route::get('/users', [UserController::class, 'index']) -> name('users.index');
-Route::get('/users/create', [UserController::class, 'create']) -> name('users.create'); //tem que ser ser antes da show
-Route::post('/users/', [UserController::class, 'store']) -> name('users.store');
-Route::get('/users/{id}', [UserController::class, 'show']) -> name('users.show');
 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+require __DIR__.'/auth.php';
